93308d8cd0d7f789a155729f361c7c5167c3f64c
Mario Segal <mario.segal@gmail.com>
2015-06-12 10:27:50 -0400

    Commit 20150612

--- OCA/Dan questions 20150611.R
+++ OCA/Dan questions 20150611.R
@@ -9,7 +9,8 @@ aux
 repeated1 %>% group_by(account_master,level_1,level_2, level_3) %>% summarise(N=n()) %>% filter(N>1) %>% group_by(N) %>% summarise(N1=n())
 
 
-analysis_base %>% group_by(account_master, level_1,level_2,level_3,type) %>% summarise(complaints=n()) %>% group_by(complaints) %>% summarise(N=n())
+ combo = analysis_base %>% group_by(account_master, level_1,level_2,level_3) %>% summarise(complaints=n()) %>% group_by(complaints) %>% summarise(N=n())
+write.table(combo,'clipboard',sep='\t',row.names=F)
 
-
-analysis_base %>% group_by(account_master, level_1,level_2,level_3,type) %>% summarise(complaints=n()) %>% filter(complaints>=2) %>%  group_by(account_master) %>% summarise(N=n()) %>% group_by(N) %>% summarise(n())
+acct1 = analysis_base %>% group_by(account_master, level_1,level_2,level_3) %>% summarise(complaints=n()) %>% filter(complaints>=2) %>%  group_by(account_master) %>% summarise(N=n()) %>% group_by(N) %>% summarise(n())
+write.table(acct1,'clipboard',sep='\t',row.names=F)
